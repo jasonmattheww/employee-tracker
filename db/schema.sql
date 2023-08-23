151121ec77cf76af0c85da0ee0d53a62273e9d40
Jason <jasontorrealba199@gmail.com>
2023-08-23 12:24:18 -0400

    Fixed errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INIT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) 
 );
 
 CREATE TABLE role (
-  id INIT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INIT NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INIT NOT NOT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INIT NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-  manager_id INIT NOT NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
