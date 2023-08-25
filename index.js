b1643d5a5150a95d9383640e979b2ca3a75f7243
Jason <jasontorrealba199@gmail.com>
2023-08-25 10:50:49 -0400

    Added view all employees function

--- index.js
+++ index.js
@@ -80,3 +80,19 @@ function viewAllRoles() {
     initPropmt();
   });
 };
+
+// View All Employees
+function viewAllEmployees() {
+  const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.department_name AS department, role.salary,
+               CONCAT(manager.first_name, ' ' ,manager.last_name) AS manager
+               FROM employee 
+               LEFT JOIN role ON employee.role_id = role.id
+               LEFT JOIN department ON role.department_id = department.id
+               LEFT JOIN employee AS manager ON employee.manager_id = manager.id`;
+               
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.table(result);
+    initPropmt();
+  });
+};
